44fd627ddedfd4d63f159e88eb1bdfbb6f9162b3
Adelar da Silva Queiróz <adelarsq@gmail.com>
2021-01-19 14:22:21 -0300

    Trim path on NERDTree

--- lua/util.lua
+++ lua/util.lua
@@ -11,14 +11,48 @@ M.Call = function(arg0, arg1)
     return api.nvim_call_function(arg0, arg1)
 end
 
-M.TrimmedDirectory = function(arg0)
-    -- local home = os.getenv("HOME")
-    -- local _, index = string.find(dir, home, 1)
-    -- if index ~= nil and index ~= string.len(dir) then
-        -- return string.gsub(dir, home, '~')
-    -- end
+local SplitString = function(arg0)
 
-    return arg0
+    -- TODO Windows support
+    local arg0Split = arg0:gmatch('[^/%s]+')
+
+    local pathTable = {}
+    local i = 1
+
+    for word in arg0Split do
+        pathTable[i]=word
+        i = i + 1
+    end
+
+    return pathTable, i
+end
+
+M.TrimmedDirectory = function(arg0)
+    local home = os.getenv("HOME")
+    local path = string.gsub(arg0,home,"~")
+
+    if path=="~" then
+        return path
+    end
+
+    local pathTable, pathTableSize = SplitString(path)
+
+    local ret=''
+
+    for j=1,pathTableSize-1,1 do
+        if j == 1 then
+            ret=ret..pathTable[j]
+        else
+            ret=ret.."/"
+            if j==pathTableSize then
+                ret=ret..pathTable[j]
+            else
+                ret=ret..pathTable[j]
+            end
+        end
+    end
+
+    return ret
 end
 
 M.IsVersion5 = function()
