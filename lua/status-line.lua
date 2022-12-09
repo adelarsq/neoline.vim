932f1da6d81fe1ff8217af8517bb9c022886c90d
Adelar da Silva Queiróz <adelarsq@gmail.com>
2022-12-09 11:32:53 -0300

    🔨 refact: Colors

--- lua/status-line.lua
+++ lua/status-line.lua
@@ -1,5 +1,6 @@
 local api = vim.api
 local util = require 'util'
+local colors = require 'colors'
 local icons = require 'icons'
 local session = require 'abduco'
 local builtin = require 'builtin'
@@ -10,49 +11,8 @@ local M = {}
 
 session.abduco_session()
 
-------------------------------------------------------------------------
--- Colors
-------------------------------------------------------------------------
-
--- Different colors for mode
--- vim.g.neoline_red = '#BF616A'
--- vim.g.neoline_yellow = '#EBCB8B'
--- vim.g.neoline_green = '#A3BE8C'
--- vim.g.neoline_blue = '#81A1C1'
--- vim.g.neoline_purple = '#B48EAD'
-
-vim.g.neoline_white = '#ffffff'
-vim.g.neoline_red = '#ff5349' -- red orange
-vim.g.neoline_orange = '#ff9326'
-vim.g.neoline_yellow = '#fe6e00' -- blaze orange
-vim.g.neoline_green = '#4CBB17' -- color Kelly
--- vim.g.neoline_green = '#55a630'
-vim.g.neoline_turquoise = '#3FE0D0'
-vim.g.neoline_aqua = '#18ffe0'
-vim.g.neoline_blue = '#31baff'
--- vim.g.neoline_blue = '#3a86ff'
-vim.g.neoline_purple = '#9d8cff'
-vim.g.neoline_green_light = '#D5F5E3'
-vim.g.neoline_purple_light = '#E8DAEF'
-vim.g.neoline_blue_light = '#D6EAF8'
-vim.g.neoline_red_light = '#FADBD8'
-
--- fg and bg
-vim.g.neoline_white_fg = '#e6e6e6'
--- vim.g.neoline_black_fg = '#282c34'
--- vim.g.neoline_bg = '#4d4d4d'
-vim.g.neoline_gray = '#cccccc'
-
-------------------------------------------------------------------------
--- Components
-------------------------------------------------------------------------
-
-vim.g.neoline_normal_fg = vim.g.neoline_gray
-vim.g.neoline_normal_bg = vim.g.neoline_white
-vim.g.neoline_activeline_bg = vim.g.neoline_blue
-vim.g.neoline_activeline_fg = '#ffffff'
-vim.g.neoline_inactiveline_bg = '#cccccc'
-vim.g.neoline_inactiveline_fg = '#ffffff'
+colors.Setup()
+icons.Setup()
 
 ------------------------------------------------------------------------
 -- Configs
@@ -94,7 +54,6 @@ end
 -- Space between components
 vim.g.neoline_blank = ' '
 
-icons.Setup()
 
 ------------------------------------------------------------------------
 --                             StatusLine                             --
