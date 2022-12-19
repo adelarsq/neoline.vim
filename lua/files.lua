54f8045b9e74e6504e03d0b88e2e0a2811519938
Adelar da Silva Queiróz <adelarsq@gmail.com>
2022-12-19 12:09:11 -0300

    Add files module

---
+++ lua/files.lua
@@ -0,0 +1,20 @@
+
+local api = vim.api
+
+local M = {}
+
+M.abbreviate_path = function(path)
+  local last_name = string.match(path, "[^/\\]+$")
+
+  local previous_folders = string.match(path, "^.+[\\/]")
+
+  local abbreviated_folders = ""
+  for folder in string.gmatch(previous_folders, "[^/\\]+") do
+    abbreviated_folders = abbreviated_folders .. string.sub(folder, 1, 1) .. "/"
+  end
+
+  return abbreviated_folders .. last_name
+end
+
+return M
+
